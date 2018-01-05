bbc2269a78ef34777daa68d79e2beabf6b3ebc22
satoshi.iwasaki <satoshi.iwasaki@bigtreetc.com>
2018-01-05 18:08:22 +0900

    move files, add docs dirs

--- docs/datas/symbols.js
+++ docs/datas/symbols.js
@@ -1,560 +1,559 @@
 var _SYMBOLS = [
     {
-        "type": "defvar",
+        "type": "Variable",
         "name": "*suppress-character-coding-errors*",
         "description": "",
         "package": "babel2.conditions",
         "file": "conditions.lisp"
     }
     , {
-        "type": "define-condition",
+        "type": "Condition",
         "name": "character-coding-error",
         "description": "",
         "package": "babel2.conditions",
         "file": "conditions.lisp"
     }
     , {
-        "type": "define-condition",
+        "type": "Condition",
         "name": "character-encoding-error",
         "description": "",
         "package": "babel2.conditions",
         "file": "conditions.lisp"
     }
     , {
-        "type": "encoding-error",
-        "name": "defun",
+        "type": "Function",
+        "name": "encoding-error",
         "description": "",
         "package": "babel2.conditions",
         "file": "conditions.lisp"
     }
     , {
-        "type": "define-condition",
+        "type": "Condition",
         "name": "character-decoding-error",
         "description": "",
         "package": "babel2.conditions",
         "file": "conditions.lisp"
     }
     , {
-        "type": "define-condition",
+        "type": "Condition",
         "name": "end-of-input-in-character",
         "description": "",
         "package": "babel2.conditions",
         "file": "conditions.lisp"
     }
     , {
-        "type": "define-condition",
+        "type": "Condition",
         "name": "character-out-of-range",
         "description": "",
         "package": "babel2.conditions",
         "file": "conditions.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "decoding-error",
         "description": "",
         "package": "babel2.conditions",
         "file": "conditions.lisp"
     }
     , {
-        "type": "define-condition",
+        "type": "Condition",
         "name": "invalid-utf8-starter-byte",
         "description": "",
         "package": "babel2.conditions",
         "file": "conditions.lisp"
     }
     , {
-        "type": "define-condition",
+        "type": "Condition",
         "name": "invalid-utf8-continuation-byte",
         "description": "",
         "package": "babel2.conditions",
         "file": "conditions.lisp"
     }
     , {
-        "type": "define-condition",
+        "type": "Condition",
         "name": "overlong-utf8-sequence",
         "description": "",
         "package": "babel2.conditions",
         "file": "conditions.lisp"
     }
     , {
-        "type": "define-condition",
+        "type": "Condition",
         "name": "invalid-gbk-byte",
         "description": "",
         "package": "babel2.conditions",
         "file": "conditions.lisp"
     }
     , {
-        "type": "define-condition",
+        "type": "Condition",
         "name": "invalid-gbk-character",
         "description": "",
         "package": "babel2.conditions",
         "file": "conditions.lisp"
     }
     , {
-        "type": "defvar",
+        "type": "Variable",
         "name": "*default-eol-style*",
         "description": "",
         "package": "babel2",
         "file": "external-format.lisp"
     }
     , {
-        "type": "defclass",
+        "type": "Class",
         "name": "external-format",
         "description": "",
         "package": "babel2",
         "file": "external-format.lisp"
     }
     , {
-        "type": "defmethod",
+        "type": "Generic Function",
         "name": "print-object",
         "description": "",
         "package": "babel2",
         "file": "external-format.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "make-external-format",
         "description": "",
         "package": "babel2",
         "file": "external-format.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "ensure-external-format",
         "description": "",
         "package": "babel2",
         "file": "external-format.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "external-format-equal",
         "description": "",
         "package": "babel2",
         "file": "external-format.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "lookup-mapping",
         "description": "",
         "package": "babel2",
         "file": "external-format.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "sharp-backslash-reader",
         "description": "",
         "package": "babel2",
         "file": "sharp-backslash.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "make-sharp-backslash-reader",
         "description": "",
         "package": "babel2",
         "file": "sharp-backslash.lisp"
     }
     , {
-        "type": "defmacro",
+        "type": "Macro",
         "name": "enable-sharp-backslash-syntax",
         "description": "",
         "package": "babel2",
         "file": "sharp-backslash.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "set-sharp-backslash-syntax-in-readtable",
         "description": "",
         "package": "babel2",
         "file": "sharp-backslash.lisp"
     }
     , {
-        "type": "defmacro",
+        "type": "Macro",
         "name": "ub-get",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defmacro",
+        "type": "Macro",
         "name": "ub-set",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defmacro",
+        "type": "Macro",
         "name": "string-get",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defmacro",
+        "type": "Macro",
         "name": "string-set",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defconstant",
+        "type": "Constant Variable",
         "name": "unicode-char-code-limit",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defparameter",
+        "type": "Variable",
         "name": "*string-vector-mappings*",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defparameter",
+        "type": "Variable",
         "name": "*simple-base-string-vector-mappings*",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "check-vector-bounds",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defmacro",
+        "type": "Macro",
         "name": "with-simple-vector",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defmacro",
+        "type": "Macro",
         "name": "with-checked-simple-vector",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "octets-to-string",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "octets2string",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "bom-vector",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "string-to-octets",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "string2octets",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "concatenate-strings-to-octets",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "string-size-in-octets",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "vector-size-in-chars",
         "description": "",
         "package": "babel2",
         "file": "strings.lisp"
     }
     , {
-        "type": "deftype",
+        "type": "Type",
         "name": "eol-style",
         "description": "",
         "package": "babel2.types",
         "file": "types.lisp"
     }
     , {
-        "type": "deftype",
+        "type": "Type",
         "name": "unicode-char",
         "description": "",
         "package": "babel2.types",
         "file": "types.lisp"
     }
     , {
-        "type": "deftype",
+        "type": "Type",
         "name": "simple-unicode-string",
         "description": "",
         "package": "babel2.types",
         "file": "types.lisp"
     }
     , {
-        "type": "deftype",
+        "type": "Type",
         "name": "unicode-string",
         "description": "",
         "package": "babel2.types",
         "file": "types.lisp"
     }
     , {
-        "type": "defgeneric",
+        "type": "Generic Function",
         "name": "get-output-stream-sequence",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "vector-extend",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defclass",
+        "type": "Class",
         "name": "in-memory-stream",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defmethod",
+        "type": "Generic Function",
         "name": "stream-element-type",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "stream-accepts-octets?",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "stream-accepts-characters?",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defclass",
+        "type": "Class",
         "name": "in-memory-input-stream",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defclass",
+        "type": "Class",
         "name": "in-memory-output-stream",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "make-in-memory-output-stream",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "make-in-memory-input-stream",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defclass",
+        "type": "Class",
         "name": "vector-stream",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
-    /* xxx */
     , {
-        "type": "defclass",
+        "type": "Class",
         "name": "vector-input-stream",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defclass",
+        "type": "Class",
         "name": "vector-output-stream",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "define-condition",
+        "type": "Condition",
         "name": "in-memory-stream-error",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "define-condition",
+        "type": "Condition",
         "name": "in-memory-stream-closed-error",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "define-condition",
+        "type": "Condition",
         "name": "wrong-element-type-stream-error",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "wrong-element-type-stream-error",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "check-if-open",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "check-if-accepts-octets",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "check-if-accepts-characters",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defmethod",
+        "type": "Generic Function",
         "name": "stream-read-byte",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defmethod",
+        "type": "Generic Function",
         "name": "stream-listen",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defmethod",
+        "type": "Generic Function",
         "name": "stream-read-sequence",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defmethod",
+        "type": "Generic Function",
         "name": "stream-write-byte",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "extend-vector-output-stream-buffer",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defmethod",
+        "type": "Generic Function",
         "name": "stream-write-char",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defmethod",
+        "type": "Generic Function",
         "name": "stream-write-sequence",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defmethod",
+        "type": "Generic Function",
         "name": "stream-write-string",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defmethod",
+        "type": "Generic Function",
         "name": "stream-line-column",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defmethod",
+        "type": "Generic Function",
         "name": "stream-file-position",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defun",
+        "type": "Function",
         "name": "make-vector-stream-buffer",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defmethod",
+        "type": "Generic Function",
         "name": "get-output-stream-sequence",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defmacro",
+        "type": "Macro",
         "name": "with-output-to-sequence",
         "description": "",
         "package": "babel2-streams",
         "file": "streams.lisp"
     }
     , {
-        "type": "defmacro",
+        "type": "Macro",
         "name": "with-input-from-sequence",
         "description": "",
         "package": "babel2-streams",
